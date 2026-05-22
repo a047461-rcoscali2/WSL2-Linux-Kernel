dd9a588f27109d2a7724d4f714e812f6e31767ac
Rémi COHEN SCALI <remi.cohen-scali@ampere.cars>
2026-05-22 14:08:45 +0200

    feat(scripts): Improve modules VHDX generation with sudo control and slack
    Rework the `gen_modules_vhdx.sh` script to explicitly manage `sudo` usage for privileged operations via the `USE_SUDO` environment variable. This provides more control to the caller on how elevated privileges are handled.
    
    Increase the VHDX image slack space from 256 MiB to 320 MiB to provide additional buffer and prevent potential out-of-space issues.
    
    Add `*.vhdx` to `.gitignore` to exclude generated VHDX files from source control.

--- Microsoft/scripts/gen_modules_vhdx.sh
+++ Microsoft/scripts/gen_modules_vhdx.sh
@@ -11,10 +11,27 @@ if [ -e "$3" ]; then
 	exit 2
 fi
 
+if test -z "$USE_SUDO";
+then
+    echo "Define the USE_SUDO env var to specify if you want to use or not sudo" 2>
+    echo "For using 'sudo' define $ export USE_SUDO=yes" 2>
+    echo "For not using 'sudo' define export USE_SUDO=no (but you'll have to 'sudo gen_modules_vhdx.sh ...')" 2>
+else
+    if test "$USE_SUDO" == "yes"
+    then
+        SUDO_CMD=sudo
+    else if test "$USE_SUDO" == "no"
+         then
+             SUDO_CMD=
+         else
+             echo "USE_SUDO shall be 'yes' or 'no'"
+         fi
+    fi
+fi
 
 # Calculate modules size (+ 256MiB for slack)
 modules_size=$(du -bs "$1" | awk '{print $1;}')
-modules_size=$((modules_size + (256*(1<<20))))
+modules_size=$((modules_size + (320*(1<<20))))
 
 # Create our scratch directory
 tmp_dir=$(mktemp -d)
@@ -23,21 +40,22 @@ tmp_dir=$(mktemp -d)
 dd if=/dev/zero of="$tmp_dir/modules.img" bs=1024 count=$((modules_size / 1024))
 
 # Set up fs and mount
-lo_dev=$(losetup --find --show "$tmp_dir/modules.img")
-mkfs -t ext4 "$lo_dev"
+lo_dev=$($SUDO_CMD losetup --find --show "$tmp_dir/modules.img")
+$SUDO_CMD mkfs -t ext4 "$lo_dev"
 mkdir "$tmp_dir/modules_img"
-mount "$lo_dev" "$tmp_dir/modules_img"
-chmod a+rw "$tmp_dir/modules_img"
+$SUDO_CMD mount "$lo_dev" "$tmp_dir/modules_img"
+$SUDO_CMD chmod a+rw "$tmp_dir/modules_img"
 
 # Copy over the contents of $1
 cp -r "$1/lib/modules/$2"/* "$tmp_dir/modules_img"
-umount "$tmp_dir/modules_img"
+sync
+$SUDO_CMD umount "$tmp_dir/modules_img"
 
 # Do the final conversion
 qemu-img convert -O vhdx "$tmp_dir/modules.img" "$3"
 
-# Fix ownership since we're probably running under sudo
-if [ -n "$SUDO_USER" ]; then
+# Fix ownership since we're running under sudo
+if [ "$USE_SUDO" == "no" ]; then
 	chown "$SUDO_USER:$SUDO_USER" "$3"
 fi
 
